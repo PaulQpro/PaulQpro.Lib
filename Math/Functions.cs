fe5075476079f8c4730b78e2797c171be3ad9801
PaulQpro <PaulQpro@mail.ru>
2023-05-13 10:21:33 +0300

    Added Functions.cs

---
+++ Math/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulQpro.Lib.Math
+{
+    public static class Functions
+    {
+        static public double Sum(int n, int m, Func<double, double> f)
+        {
+            double result = n;
+            for (int i = n+1; i <= m; i++)
+            {
+                result += f(i);
+            }
+            return result;
+        }
+        static public double Prod(int n, int m, Func<double, double> f)
+        {
+            double result = n;
+            for (int i = n+1; i <= m; i++)
+            {
+                result *= f(i);
+            }
+            return result;
+        }
+    }
+}
