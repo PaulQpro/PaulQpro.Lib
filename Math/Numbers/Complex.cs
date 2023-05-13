c3bf49e4c96266281f17d913c0deeabb7ca190b1
PaulQpro <PaulQpro@mail.ru>
2023-05-13 10:22:02 +0300

    Modified Complex.cs

--- Math/Numbers/Complex.cs
+++ Math/Numbers/Complex.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PaulQpro.Lib.Math.LinearMath;
+using static System.Math;
 
 namespace PaulQpro.Lib.Math.Numbers
 {
-    class Complex
+    public class Complex
     {
         public double Real { get; }
         public double Imaginary { get; }
 
+        public static Complex operator + (Complex a, Complex b) => new Complex(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        public static Complex operator -(Complex a, Complex b) => new Complex(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        public static Complex operator *(Complex a, Complex b) => new Complex(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Imaginary * b.Real + a.Real * b.Imaginary);
+        public static Complex operator /(Complex a, Complex b) => new Complex((a.Real * b.Real + a.Imaginary * b.Imaginary) / (Pow(b.Real, 2) + Pow(b.Imaginary, 2)), (a.Imaginary * b.Real - a.Real * b.Imaginary) / (Pow(b.Real, 2) + Pow(b.Imaginary, 2)));
+
+        public static implicit operator string(Complex number) => number.ToString();
+        public static explicit operator VectorNum2(Complex number) => new double[] { number.Real, number.Imaginary };
+
+        public override string ToString()
+        {
+            if (Real == 0 && Imaginary != 0) return $"({Imaginary}i)";
+            else if (Imaginary == 0) return $"({Real})";
+            else return $"({Real} + {Imaginary}i)";
+        }
+
+        public Complex(double Re, double Im)
+        {
+            Real = Re;
+            Imaginary = Im;
+        }
     }
 }
