544befb029467f335ad0320f51a829fa14d64df6
PaulQpro <PaulQpro@mail.ru>
2023-06-22 14:35:30 +0300

    Modified VectorInt

--- Math/LinearMath/VectorInt.cs
+++ Math/LinearMath/VectorInt.cs
@@ -1,7 +1,21 @@
-﻿namespace PaulQpro.Lib.Math.LinearMath
+﻿using Maths = System.Math;
+
+namespace PaulQpro.Lib.Math.LinearMath
 {
     public class VectorInt : Vector<int>
     {
+        public double Length { 
+            get{
+                double result = 0;
+                foreach(int val in vector)
+                {
+                    result += Maths.Pow(val, 2);
+                }
+                result = Maths.Sqrt(result);
+                return result;
+            } 
+        }
+
         public static implicit operator VectorInt(int[] values) => new VectorInt(values);
         public static explicit operator VectorInt(double[] values) => new VectorInt(values);
         public static implicit operator VectorNum(VectorInt vector) => vector.vector;
