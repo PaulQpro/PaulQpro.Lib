b6010b8ac1698a1805a4701082780ae634e279d0
PaulQpro <PaulQpro@mail.ru>
2023-06-24 13:30:43 +0300

    Generalized Set

--- Math/Sets/Set.cs
+++ Math/Sets/Set.cs
@@ -6,9 +6,38 @@ using System.Threading.Tasks;
 
 namespace PaulQpro.Lib.Math.Sets
 {
-    public class Set
+    public class Set<T>
     {
-        private List<double> Values { get; }
+        protected List<T> Values { get; set; }
+        public int Count { get; protected set; }
 
+        public bool Has(T value)
+        {
+            foreach (T Value in Values)
+            {
+                if (Value.Equals(value)) return true;
+            }
+            return false;
+        }
+        ///<summary>index must be zero or positive</summary>
+        /// <param name="index">Must be not negative</param>
+        public T this[int index]
+        {
+            get {
+                if (index >= 0) return Values[index];
+                else throw new ArgumentException();
+            }
+        }
+        public static implicit operator Set<T>(List<T> values) => new Set<T>(values);
+        public static implicit operator Set<T>(T[] values) => new Set<T>(new List<T>(values));
+        public static implicit operator List<T>(Set<T> value) => value.Values;
+        public static implicit operator T[](Set<T> value) => value.Values.ToArray();
+
+        protected Set(List<T> values)
+        {
+            Values = values;
+            Count = values.Count;
+        }
+        protected Set() { }
     }
 }
