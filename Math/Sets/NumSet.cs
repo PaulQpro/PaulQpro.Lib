e90741ed69a1785cf5d0d0d21f382c765fae3a8d
PaulQpro <PaulQpro@mail.ru>
2023-06-24 13:30:30 +0300

    Replaced Set with NumSet

---
+++ Math/Sets/NumSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulQpro.Lib.Math.Sets
+{
+    public class NumSet : Set<double>
+    {
+        public static implicit operator NumSet(List<double> values) => new NumSet(values);
+        public static implicit operator NumSet(double[] values) => new NumSet(new List<double>(values));
+        public static implicit operator List<double>(NumSet value) => value.Values;
+        public static implicit operator double[](NumSet value) => value.Values.ToArray();
+
+        private NumSet(List<double> values)
+        {
+            Values = values;
+            Count = values.Count;
+        }
+    }
+}
