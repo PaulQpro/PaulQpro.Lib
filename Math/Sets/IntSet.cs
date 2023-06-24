80154d8f14ee0936573a02343e768b65ae2155c4
PaulQpro <PaulQpro@mail.ru>
2023-06-24 13:36:22 +0300

    Added IntSet

---
+++ Math/Sets/IntSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulQpro.Lib.Math.Sets
+{
+    public class IntSet : Set<int>
+    {
+        public static implicit operator IntSet(List<int> values) => new IntSet(values);
+        public static implicit operator IntSet(int[] values) => new IntSet(new List<int>(values));
+        public static implicit operator List<int>(IntSet value) => value.Values;
+        public static implicit operator int[](IntSet value) => value.Values.ToArray();
+
+        private IntSet(List<int> values)
+        {
+            Values = values;
+            Count = values.Count;
+        }
+    }
+}
